101df0ea31b1e7e0bb183313bfc95ef5732921c9
at@atta <athallah.rafi13@gmail.com>
2025-04-14 22:33:09 +0700

    created .env file.

--- app/app.py
+++ app/app.py
@@ -1,11 +1,28 @@
 from flask import Flask, render_template, request
+import os
 import firebase_admin
 from firebase_admin import credentials, firestore
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 
+
+load_dotenv()
+cred_dict = {
+    "type": os.getenv("TYPE"),
+    "project_id": os.getenv("PROJECT_ID"),
+    "private_key_id": os.getenv("PRIVATE_KEY_ID"),
+    "private_key": os.getenv("PRIVATE_KEY").replace("\\n", "\n"),
+    "client_email": os.getenv("CLIENT_EMAIL"),
+    "client_id": os.getenv("CLIENT_ID"),
+    "auth_uri": os.getenv("AUTH_URI"),
+    "token_uri": os.getenv("TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("AUTH_PROVIDER_CERT_URL"),
+    "client_x509_cert_url": os.getenv("CLIENT_CERT_URL"),
+}
+
 # Initialize Firebase
-cred = credentials.Certificate('recipe-page-project-firebase-adminsdk.json')
+cred = credentials.Certificate(cred_dict)
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 recipes_ref = db.collection('recipes')
